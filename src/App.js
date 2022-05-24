a0740b96ddc092febd684dafb646a8d17c110691
zahrabola <95353397+zahrabola@users.noreply.github.com>
2022-05-24 12:11:34 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const fetchItems = async () => {
 const result = await axios(`https://www.breakingbadapi.com/api/characters`)
 
 console.log(result.data)
+setItems(result.data)
+setLoading(false)
 }
 fetchItems()
 }, [])
